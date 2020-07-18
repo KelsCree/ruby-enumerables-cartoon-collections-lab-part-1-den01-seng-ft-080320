cba99f264de3334fd3c8757034447751003cd0d5
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-18 21:24:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element|
+    index = 0
     puts element[index + 1]
+    index += 1
   end
 end
